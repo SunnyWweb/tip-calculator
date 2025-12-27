3ec5946a36487b6d5f18de246b9612944afc4010
SunnyWweb <sunny.wweb@gmail.com>
2025-12-26 21:44:47 -0500

    Tip calculator project

---
+++ tip_calculator.py
@@ -0,0 +1,10 @@
+print("Welcome to the tip calculator!")
+total_bill = input("What was the total bill?: $")
+tip = input("""How much of a tip would you like to give? 
+10%, 12%, or 15%? *just type the number*: """)
+people = input("How many people to split the bill?: ")
+payper_person = ((float(total_bill) * (float(tip)/100) + float(total_bill))) / float(people)
+print(f"Each person should pay: ${round(payper_person, 2)}")
+
+#you could also do print(f"Each person should pay: ${pay_per_person:.2f}") which always gives two decimals
+#this is a later concept though so don't use it now
